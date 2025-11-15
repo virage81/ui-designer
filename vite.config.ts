d1480b4119267748acaee762f38a3b55be7d4611
virage81 <karimovminds@gmail.com>
2025-11-15 20:43:56 +0400

    feat: add folder structure and aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
+const root = path.resolve(__dirname, 'src');
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			'@components': `${root}/components`,
+			'@pages': `${root}/pages`,
+			'@store': `${root}/store`,
+			'@shared': `${root}/shared`,
+			'@': root,
+		},
+	},
 });
