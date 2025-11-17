2c24092b22093e76ba991f6239a633c8bab45f7d
virage81 <karimovminds@gmail.com>
2025-11-17 22:17:00 +0400

    feat(#16): setup jest and add folder structure

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+const config = {
+	preset: 'ts-jest',
+	transform: {
+		'^.+\\.(ts|tsx|js|jsx)$': 'ts-jest',
+	},
+	testEnvironment: 'jsdom',
+	transformIgnorePatterns: ['node_modules'],
+	setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+	moduleNameMapper: {
+		'\\.(css|less|png)$': '<rootDir>/spec/__mocks__/styleMock.js',
+		'src(.*)$': '<rootDir>/src$1',
+	},
+};
+
+export default config;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+const rootReducer = combineReducers({});
 
 export const store = configureStore({
-	reducer: {},
+	reducer: rootReducer,
+	devTools: true,
 });
 
+export const setupStore = (preloadedState?: Partial<RootState>) => {
+	return configureStore({
+		reducer: rootReducer,
+		preloadedState,
+	});
+};
+
 export type RootState = ReturnType<typeof store.getState>;
+export type AppStore = ReturnType<typeof setupStore>;
 export type AppDispatch = typeof store.dispatch;
