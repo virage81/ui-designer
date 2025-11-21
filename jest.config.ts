e7a39fff8614d9bc2b16b120a466ef8a04b4c804
virage81 <karimovminds@gmail.com>
2025-11-21 23:03:07 +0400

    chore: add aliases for jest

--- jest.config.ts
+++ jest.config.ts
@@ -7,8 +7,12 @@ const config = {
 	transformIgnorePatterns: ['node_modules'],
 	setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 	moduleNameMapper: {
-		'\\.(css|less|png)$': '<rootDir>/spec/__mocks__/styleMock.js',
+		'\\.(css|less|png)$': '<rootDir>/tests/__mocks__/styleMock.js',
 		'^@/(.*)$': '<rootDir>/src/$1',
+		'^@pages/(.*)$': '<rootDir>/src/pages/$1',
+		'^@components/(.*)$': '<rootDir>/src/components/$1',
+		'^@store/(.*)$': '<rootDir>/src/store/$1',
+		'^@shared/(.*)$': '<rootDir>/src/shared/$1',
 	},
 };
 
