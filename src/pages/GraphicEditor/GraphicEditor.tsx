95df6a4017678207324ba0b53711e6322b332c02
Alexander Vasilev <46332577+rayonnant@users.noreply.github.com>
2025-11-22 21:45:45 +0300

    feat: add fallback to 404 if projects route doesn't exist

--- src/pages/GraphicEditor/GraphicEditor.tsx
+++ src/pages/GraphicEditor/GraphicEditor.tsx
@@ -3,8 +3,23 @@ import { LeftSidebar } from '@components/LeftSidebar';
 import { RightSideBar } from '@components/RightSidebar';
 import { TopMenu } from '@components/TopMenu';
 import { Box } from '@mui/material';
+import type {RootState} from "@/store";
+import type {Project} from "@shared/types/project.ts";
+import {Navigate, useParams} from "react-router-dom";
+import {useSelector} from "react-redux";
 
 export const GraphicEditor: React.FC = () => {
+	const selectProjectExists = (state: RootState, id: string) => {
+		return state.projects.projects.some((p: Project) => p.id === id);
+	}
+
+	const { id } = useParams<{ id: string }>();
+	const exists: boolean = useSelector((state: RootState) => selectProjectExists(state, id ?? ''));
+
+	if (!exists) {
+		return <Navigate to="/404" replace />;
+	}
+
 	return (
 		<div>
 			<TopMenu />
