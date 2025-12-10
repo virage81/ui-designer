e75d51ed6aeab68d2f0e3b1b5b9881a9834aa727
Bulat <64682284+virage81@users.noreply.github.com>
2025-12-10 22:13:17 +0400

    Revert "Revert "Full history""

--- src/components/LeftSidebar/components/ToolsHistoryGroup/ToolsHistoryGroup.tsx
+++ src/components/LeftSidebar/components/ToolsHistoryGroup/ToolsHistoryGroup.tsx
@@ -1,18 +1,28 @@
 import { Box, Button } from '@mui/material';
+import type { RootState } from '@store/index';
+import { isRedoActiveSelector, isUndoActiveSelector, redoHistory, undoHistory } from '@store/slices/projectsSlice';
 import { ACTIONS } from '@store/slices/toolsSlice';
 import { Redo2Icon, Undo2Icon } from 'lucide-react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 const HISTORY_TOOLS = [
-	{ id: ACTIONS.UNDO, icon: <Undo2Icon size={16} color={'var(--color)'} />, label: 'Назад', isDisabled: false },
+	{
+		id: ACTIONS.UNDO,
+		label: 'Отменить',
+	},
 	{
 		id: ACTIONS.REDO,
-		icon: <Redo2Icon size={16} color={'var(--color-muted)'} />,
-		label: 'Вперед',
-		isDisabled: true,
+		label: 'Вернуть',
 	},
 ];
 
 export const ToolsHistoryGroup: React.FC = () => {
+	const dispatch = useDispatch();
+	const { id: projectId = '' } = useParams();
+	const isUndoActive = useSelector((state: RootState) => isUndoActiveSelector(state, projectId));
+	const isRedoActive = useSelector((state: RootState) => isRedoActiveSelector(state, projectId));
+
 	return (
 		<Box>
 			{HISTORY_TOOLS.map(tool => (
@@ -20,12 +30,26 @@ export const ToolsHistoryGroup: React.FC = () => {
 					key={tool.id}
 					variant='graphic-tools'
 					title={tool.label}
-					disabled={tool.isDisabled}
-					onClick={e => {
-						console.log('click:', e);
-					}}>
-					{tool.icon}
-				</Button>
+					disabled={tool.id === ACTIONS.UNDO ? !isUndoActive : !isRedoActive}
+					startIcon={
+						tool.id === ACTIONS.UNDO ? (
+							<Undo2Icon size={16} color={isUndoActive ? 'var(--color)' : 'var(--color-muted)'} />
+						) : (
+							<Redo2Icon size={16} color={isRedoActive ? 'var(--color)' : 'var(--color-muted)'} />
+						)
+					}
+					onClick={() =>
+						dispatch(
+							tool.id === ACTIONS.UNDO
+								? undoHistory({
+										projectId: projectId,
+								  })
+								: redoHistory({
+										projectId: projectId,
+								  }),
+						)
+					}
+				/>
 			))}
 		</Box>
 	);
