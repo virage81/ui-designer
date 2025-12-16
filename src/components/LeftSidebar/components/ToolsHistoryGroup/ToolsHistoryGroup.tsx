7a1ac6b07fc4f78c7a35ce0cee4d5e97992f0db6
Afonin Sergey <afoninsergey@mail.ru>
2025-12-16 09:14:38 +0300

    feat(#178): Implements keyboard shortcuts "Ctrl+Z" Undo, "Ctrl+Shift+Z" Redo

--- src/components/LeftSidebar/components/ToolsHistoryGroup/ToolsHistoryGroup.tsx
+++ src/components/LeftSidebar/components/ToolsHistoryGroup/ToolsHistoryGroup.tsx
@@ -3,17 +3,18 @@ import type { RootState } from '@store/index';
 import { isRedoActiveSelector, isUndoActiveSelector, redoHistory, undoHistory } from '@store/slices/projectsSlice';
 import { ACTIONS } from '@store/slices/toolsSlice';
 import { Redo2Icon, Undo2Icon } from 'lucide-react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 const HISTORY_TOOLS = [
 	{
 		id: ACTIONS.UNDO,
-		label: 'Отменить',
+		label: 'Отменить (Ctrl+Z)',
 	},
 	{
 		id: ACTIONS.REDO,
-		label: 'Вернуть',
+		label: 'Вернуть (Ctrl+Shift+Z)',
 	},
 ];
 
@@ -22,6 +23,40 @@ export const ToolsHistoryGroup: React.FC = () => {
 	const { id: projectId = '' } = useParams();
 	const isUndoActive = useSelector((state: RootState) => isUndoActiveSelector(state, projectId));
 	const isRedoActive = useSelector((state: RootState) => isRedoActiveSelector(state, projectId));
+
+	useEffect(() => {
+		const handleKeyDown = (e: KeyboardEvent) => {
+			const target = e.target as HTMLElement;
+			if (target.tagName === 'INPUT' || target.tagName === 'TEXTAREA') {
+				return;
+			}
+
+			if (e.ctrlKey && !e.shiftKey && !e.altKey && e.code === 'KeyZ') {
+				e.preventDefault();
+				if (!isUndoActive) return;
+	
+				dispatch(
+					undoHistory({
+						projectId,
+					}),
+				);
+			}
+
+			if (e.ctrlKey && e.shiftKey && !e.altKey && e.code === 'KeyZ') {
+				e.preventDefault();
+				if (!isRedoActive) return;
+
+				dispatch(
+					redoHistory({
+						projectId,
+					}),
+				);
+			}
+		};
+
+		window.addEventListener('keydown', handleKeyDown);
+		return () => window.removeEventListener('keydown', handleKeyDown);
+	}, [dispatch, projectId, isUndoActive, isRedoActive]);
 
 	return (
 		<Box>
